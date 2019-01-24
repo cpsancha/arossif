87d595c3e2e94bf9aafe3dab7b3134934d916e52
Carlos Pérez Sancha <carlos.perez@deimos-space.com>
2019-01-24 17:26:14 +0100

    JUnit ported from Junit3 to JUnit5 and some dummy tests added

--- src/test/java/app/AppTest.java
+++ src/test/java/app/AppTest.java
@@ -1,38 +1,52 @@
 package app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all test methods");
     }
+ 
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each test method");
+    }
+ 
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each test method");
+    }
+ 
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all test methods");
+    }	
+    
+    @Test
+    @DisplayName("First test")
+    void firstTest() {
+        System.out.println("First test method");
+    }
+ 
+    @Test
+    @DisplayName("Second test")
+    void secondTest() {
+        System.out.println("Second test method");
+    }
+	
+	
+	
+	
+	
 }
