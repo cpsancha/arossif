0c3edf5140e79db2c750419cff8546e9a2cf9e97
Sork <sork93@gmail.com>
2019-04-22 20:16:12 +0200

    Migrate the application to the Java Platform Module System (single module for all the app)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module arossif {
+	exports app;
+	exports utils;
+	exports gui;
+	exports db;
+
+	requires java.sql;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires org.apache.logging.log4j;
+}
